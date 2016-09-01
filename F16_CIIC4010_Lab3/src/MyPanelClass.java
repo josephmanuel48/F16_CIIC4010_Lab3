8ed46ed8db40bfebd4c0fcf6cf5550af290be2f8
4010d7 <4010d7@parcha.ece.uprm.edu>
2016-09-01 15:51:10 -0400

    STAR

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -46,11 +46,37 @@ public class MyPanelClass extends JPanel {
                         //g.drawLine(x1,y2, x2, y1);
                         
                         //Draw oval centered 
-                        g.setColor(Color.LIGHT_GRAY);
-                        g.fillOval((getWidth()-55)/2,(getHeight()-55)/2,55, 55); 
+                        //g.setColor(Color.LIGHT_GRAY);
+                        //g.fillOval((getWidth()-55)/2,(getHeight()-55)/2,55, 55); 
                         
+                        //Draw the arrow 
+//                        Polygon p = new Polygon();
+//                        p.addPoint(x1 + 5, y1 + 25);
+//                        p.addPoint(x1 + 20, y1 + 10);
+//                        p.addPoint(x1 + 35, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 25);
+//                        g.setColor(Color.YELLOW);
+//                        g.fillPolygon(p);
+
+                        //Draw the star
+                        Polygon p2 = new Polygon();
+                        p2.addPoint(x1 + 25, y1 + 73);
+                        p2.addPoint(x1 + 41, y1 + 73);
+                        p2.addPoint(x1 + 47, y1 + 58);
+                        p2.addPoint(x1 + 53, y1 + 73);
+                        p2.addPoint(x1 + 69, y1 + 73);
+                        p2.addPoint(x1 + 56, y1 + 83);
+                        p2.addPoint(x1 + 61, y1 + 98);
+                        p2.addPoint(x1 + 47, y1 + 88);
+                        p2.addPoint(x1 + 34, y1 + 98);
+                        p2.addPoint(x1 + 38, y1 + 83);
+                        g.setColor(Color.WHITE);
+                        g.fillPolygon(p2);
                         
-            }
+			}
 }
 
 
